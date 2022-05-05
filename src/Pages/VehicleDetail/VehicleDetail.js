1e92cdb797c74b5e1bb5d50b2724fb60bbf04159
Farhan <farhansafin@gmail.com>
2022-05-05 17:27:30 +0600

    Patch partially done

--- src/Pages/VehicleDetail/VehicleDetail.js
+++ src/Pages/VehicleDetail/VehicleDetail.js
@@ -10,11 +10,24 @@ const VehicleDetail = () => {
         fetch(url)
         .then(res => res.json())
         .then(data => setVehicle(data));
-    }, [])
+    }, []);
+
+
+
+    const updateQuantity = id => {
+        const vehicleQuantity = parseInt(vehicle.quantity) - 1;
+        console.log(vehicleQuantity);
+        const url = `http://localhost:5000/vehicle/${id}`;
+            fetch(url, {
+                method:'PATCH'
+            })
+            .then(res => res.json())
+            .then(data => {console.log(data)});
+    }
+
     return (
         <div>
-            
-            <img src={vehicle.image} alt="" srcset="" />
+            <img src={vehicle.image} alt=""/>
             <h2>ID: {vehicleId}</h2>
             <h2>Vehicle: {vehicle.name}</h2>
             <h2>Desc: {vehicle.description}</h2>
@@ -22,6 +35,7 @@ const VehicleDetail = () => {
             <h2>Quantity: {vehicle.quantity}</h2>
             <h2>Supplier: {vehicle.supplier}</h2>
             <h2>Sold: {vehicle.sold}</h2>
+            <button onClick={() => updateQuantity(vehicleId)}>Delivered: {vehicle.name}</button>
         </div>
     );
 };
