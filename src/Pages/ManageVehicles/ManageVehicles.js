775167c2e83bda16c40d1ede7090a29ae6c0188b
Farhan <farhansafin@gmail.com>
2022-05-07 15:03:12 +0600

    My item and manage item page designed

--- src/Pages/ManageVehicles/ManageVehicles.js
+++ src/Pages/ManageVehicles/ManageVehicles.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import useVehicles from '../../hooks/useVehicles';
+import './ManageVehicle.css'
 
 const ManageVehicles = () => {
     const [vehicles,setVehicles] = useVehicles();
@@ -31,13 +32,40 @@ const ManageVehicles = () => {
     
     return (
         <div>
-            <h2>Manage your all cars</h2>
+            <h2 className='text-center mt-5'>Manage your all cars</h2>
+            <table className="table table-dark mt-5 container">
+            <thead>
+    <tr>
+      <th className='text-center' scope="col">Items</th>
+    </tr>
+  </thead>
+            </table>
+
             {
-                vehicles.map(vehicle => <div key={vehicle._id}>
-                    <img src={vehicle.image} alt="" className='w-25' />
-                    <h4>{vehicle.name}<button onClick={() => handleDelete(vehicle._id)}>X</button>
-                    <button onClick={() => showVehicleDetail(vehicle._id)}>Manage</button>
-                    </h4>
+                vehicles.map(vehicle => <div key={vehicle._id}>  
+
+                    <table className="table container">
+  <tbody>
+    <tr>
+      <td><img src={vehicle.image} className='image' alt="" /></td>
+      <td className='text-center w-100'>Name: {vehicle.name}
+      </td>
+      <td className='d-flex'>
+      <button className='btn btn-success me-2' onClick={() => showVehicleDetail(vehicle._id)}><svg xmlns="http://www.w3.org/2000/svg" className='icon' viewBox="0 0 20 20" fill="currentColor">
+  <path d="M5 4a1 1 0 00-2 0v7.268a2 2 0 000 3.464V16a1 1 0 102 0v-1.268a2 2 0 000-3.464V4zM11 4a1 1 0 10-2 0v1.268a2 2 0 000 3.464V16a1 1 0 102 0V8.732a2 2 0 000-3.464V4zM16 3a1 1 0 011 1v7.268a2 2 0 010 3.464V16a1 1 0 11-2 0v-1.268a2 2 0 010-3.464V4a1 1 0 011-1z" />
+</svg></button>
+      <button className='btn btn-danger' onClick={() => handleDelete(vehicle._id)}>            <svg xmlns="http://www.w3.org/2000/svg" className='icon' viewBox="0 0 20 20" fill="currentColor">
+  <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clipRule="evenodd" />
+</svg></button>
+      </td>
+
+      
+
+      
+    </tr>
+  </tbody>
+</table>
+
                 </div>)
             }
             <Link to="/addvehicle"><button>Add new Vehicle</button></Link>
--- src/Pages/MyItems/MyItems.js
+++ src/Pages/MyItems/MyItems.js
@@ -2,6 +2,7 @@ import React, {  useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
 import Loading from '../Shared/Loading/Loading';
+import './MyItems.css'
 
 
 
@@ -40,13 +41,33 @@ const MyItems = () => {
     }
 
     return (
-        <div>
-            <h2>Name: {user.email}</h2>
+        <div className=''>
+            <h2 className='text-center mt-4'>Admin's Email: {user.email}</h2>
+            <table className="table table-dark mt-5 container">
+            <thead>
+    <tr>
+      <th className='text-center'>Items</th>
+    </tr>
+  </thead>
+            </table>
+
             {
                 vehicles.map(vehicle => 
                 <div key={vehicle._id}>
-                    <h4>{vehicle.name}</h4>
-                    <button onClick={() => handleDelete(vehicle._id)}>X</button>
+                <table className="table container">
+  <tbody>
+    <tr>
+      <td className='w-100'>Name: {vehicle.name}</td>
+      <td>
+      <button className='btn btn-danger' onClick={() => handleDelete(vehicle._id)}>            <svg xmlns="http://www.w3.org/2000/svg" className='icon' viewBox="0 0 20 20" fill="currentColor">
+  <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clipRule="evenodd" />
+</svg></button>
+      </td>
+
+    </tr>
+  </tbody>
+</table>
+                
                 </div>)
             }
             
