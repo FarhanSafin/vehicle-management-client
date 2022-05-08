e9de94303315ef6ff3d8406490b372cdeb849025
Farhan <farhansafin@gmail.com>
2022-05-08 15:29:07 +0600

    Table redesigned

--- src/Pages/MyItems/MyItems.js
+++ src/Pages/MyItems/MyItems.js
@@ -68,7 +68,7 @@ const MyItems = () => {
   <tbody>
     <tr>
       <td className='w-100'>Name: {vehicle.name}</td>
-      <td className='d-flex'>
+      <td className='d-flex border border-secondary'>
       <button className='btn btn-success me-2' onClick={() => showVehicleDetail(vehicle._id)}><svg xmlns="http://www.w3.org/2000/svg" className='icon' viewBox="0 0 20 20" fill="currentColor">
   <path d="M5 4a1 1 0 00-2 0v7.268a2 2 0 000 3.464V16a1 1 0 102 0v-1.268a2 2 0 000-3.464V4zM11 4a1 1 0 10-2 0v1.268a2 2 0 000 3.464V16a1 1 0 102 0V8.732a2 2 0 000-3.464V4zM16 3a1 1 0 011 1v7.268a2 2 0 010 3.464V16a1 1 0 11-2 0v-1.268a2 2 0 010-3.464V4a1 1 0 011-1z" />
 </svg></button>
