9be8b668659272d6f527bd239bdf254fe393cf97
Farhan <farhansafin@gmail.com>
2022-05-08 17:57:22 +0600

    Spinner Added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -2,30 +2,36 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import useVehicles from '../../../hooks/useVehicles';
 import Banner from '../Banner/Banner';
+import Loading from '../../Shared/Loading/Loading';
 import ListedVehicle from '../ListedVehicle/ListedVehicle';
+
 import './Home.css'
 
 const Home = () => {
 
     const [vechicles] = useVehicles();
     const slicedData = vechicles.slice(0,6);
-
-    return (
-        <div>
-            <Banner></Banner>
-            <div className="row home">
-                {
-                    slicedData.map(slice => <ListedVehicle
-                    key={slice._id}
-                    vehicle={slice}
-                    >
-                    </ListedVehicle>)
-                }
-                <Link className='text-center mt-5' to="/managevehicles"><button className='btn btn-secondary'>Manage All Vehicles</button></Link>
+    if(vechicles.length === 0){
+        return <Loading></Loading>
+    }
+    else{
+        return (
+            <div>
+                <Banner></Banner>
+                <div className="row home">
+                    {
+                        slicedData.map(slice => <ListedVehicle
+                        key={slice._id}
+                        vehicle={slice}
+                        >
+                        </ListedVehicle>)
+                    }
+                    <Link className='text-center mt-5' to="/managevehicles"><button className='btn btn-secondary'>Manage All Vehicles</button></Link>
+                </div>
+                
             </div>
-            
-        </div>
-    );
+        );
+    }
 };
 
 export default Home;
